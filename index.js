177e10b9e0723100491b039c15263b68128dbd55
shenef <matusik.rene@gmx.de>
2023-03-27 00:42:59 +0200

    add and run eslint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const generateSegments = (splitNames) => {
-    const gameName = "Game Name";
-    const categoryName = "Category Name";
-    const usesEmulatorTrueFalse = "False";
-    const attemptCount = 0;
-    const splitTemplate = (splitName) => `
+  const gameName = 'Game Name';
+  const categoryName = 'Category Name';
+  const usesEmulatorTrueFalse = 'False';
+  const attemptCount = 0;
+  const splitTemplate = (splitName) => `
         <Segment>
             <Name>${splitName}</Name>
             <Icon />
@@ -13,11 +13,16 @@ const generateSegments = (splitNames) => {
             <BestSegmentTime />
             <SegmentHistory />
         </Segment>`;
-  
-    const splitNamesArray = Array.isArray(splitNames) ? splitNames : splitNames.split(/[\n,]/);
-    const segments = splitNamesArray.filter(name => name.trim() !== "").map(name => splitTemplate(name.trim())).join("");
 
-    const splitFileTemplate = `<?xml version="1.0" encoding="UTF-8"?>
+  const splitNamesArray = Array.isArray(splitNames) ?
+    splitNames :
+    splitNames.split(/[\n,]/);
+  const segments = splitNamesArray
+      .filter((name) => name.trim() !== '')
+      .map((name) => splitTemplate(name.trim()))
+      .join('');
+
+  const splitFileTemplate = `<?xml version="1.0" encoding="UTF-8"?>
         <Run version="1.7.0">
             <GameIcon />
             <GameName>${gameName}</GameName>
@@ -35,42 +40,53 @@ const generateSegments = (splitNames) => {
             <Segments>${segments}</Segments>
             <AutoSplitterSettings />
         </Run>`;
-  
-    return splitFileTemplate;
-  };
-  
-  const generateSplitFile = () => {
-    const splitNamesInput = document.getElementById("splitNames");
-    const splitNames = splitNamesInput.value.split(/[,|\n]/).map(name => name.trim());
-    const splitFile = generateSegments(splitNames);
-    const formattedSplitFile = formatXml(splitFile);
-    const splitFileOutput = document.getElementById("splitFile");
-    splitFileOutput.value = formattedSplitFile;
-  };
-  
+
+  return splitFileTemplate;
+};
+
+const generateSplitFile = () => {
+  const splitNamesInput = document.getElementById('splitNames');
+  const splitNames = splitNamesInput.value
+      .split(/[,|\n]/)
+      .map((name) => name.trim());
+  const splitFile = generateSegments(splitNames);
+  const formattedSplitFile = formatXml(splitFile);
+  const splitFileOutput = document.getElementById('splitFile');
+  splitFileOutput.value = formattedSplitFile;
+};
+
+/**
+ * Formats an XML string by removing newlines and spaces between tags,
+ * and applying indentation.
+ * @param {string} xml - The XML string to format.
+ * @return {string} The formatted XML string.
+ */
 function formatXml(xml) {
-    // Remove all the newlines and then remove all the spaces between tags
-    xml = xml.replace(/(\r\n|\n|\r)/gm, " ").replace(/>\s+</g,'><');
-    const PADDING = ' '.repeat(4); // set desired indent size here
-    const reg = /(>)(<)(\/*)/g;
-    let pad = 0;
+  // Remove all the newlines and then remove all the spaces between tags
+  xml = xml.replace(/(\r\n|\n|\r)/gm, ' ').replace(/>\s+</g, '><');
+  const PADDING = ' '.repeat(4); // set desired indent size here
+  const reg = /(>)(<)(\/*)/g;
+  let pad = 0;
 
-    xml = xml.replace(reg, '$1\r\n$2$3');
+  xml = xml.replace(reg, '$1\r\n$2$3');
 
-    return xml.split('\r\n').map((node, index) => {
+  return xml
+      .split('\r\n')
+      .map((node, index) => {
         let indent = 0;
         if (node.match(/.+<\/\w[^>]*>$/)) {
-            indent = 0;
+          indent = 0;
         } else if (node.match(/^<\/\w/) && pad > 0) {
-            pad -= 1;
+          pad -= 1;
         } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
-            indent = 1;
+          indent = 1;
         } else {
-            indent = 0;
+          indent = 0;
         }
 
         pad += indent;
 
         return PADDING.repeat(pad - indent) + node;
-    }).join('\r\n');
+      })
+      .join('\r\n');
 }
